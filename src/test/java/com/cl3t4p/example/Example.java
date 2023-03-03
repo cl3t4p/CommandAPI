199397042cea683f78014e0cac65e848f6595975
cl3t4p <61201782+cl3t4p@users.noreply.github.com>
2023-03-03 21:32:02 +0100

    Update name

--- src/test/java/com/cl3t4p/example/Example.java
+++ src/test/java/com/cl3t4p/example/Example.java
@@ -3,13 +3,12 @@ package com.cl3t4p.example;
 import com.cl3t4p.commandapi.Commands;
 import com.cl3t4p.commandapi.annotation.CommandInfo;
 import com.cl3t4p.commandapi.annotation.CommandPermission;
-import com.cl3t4p.commandapi.annotation.Msg;
 import org.bukkit.command.CommandSender;
 
 public class Example implements Commands {
 
-    @CommandPermission("test.perm")
-    @CommandInfo(name = "village claim")
+    @CommandPermission("test.example")
+    @CommandInfo(name = "test example")
     public void start(CommandSender sender,Integer number) {
 
     }
