e29c80bdb699d31fb6a77164dbf1047fac0287d1
cl3t4p <61201782+cl3t4p@users.noreply.github.com>
2023-04-25 02:01:25 +0200

    Update example

--- src/test/java/com/cl3t4p/example/Example.java
+++ src/test/java/com/cl3t4p/example/Example.java
@@ -1,16 +1,33 @@
 package com.cl3t4p.example;
 
+
 import com.cl3t4p.commandapi.Commands;
 import com.cl3t4p.commandapi.annotation.CommandInfo;
 import com.cl3t4p.commandapi.annotation.CommandPermission;
+import com.cl3t4p.commandapi.annotation.Msg;
+import com.cl3t4p.commandapi.annotation.Tab;
 import org.bukkit.command.CommandSender;
+
+import java.util.List;
 
 public class Example implements Commands {
 
+    @Tab(value = "startTab",isMethod =true)
     @CommandPermission("test.example")
     @CommandInfo(name = "test example")
-    public void start(CommandSender sender, Integer number) {
+    public void start(CommandSender sender,@Msg("in_cooldown") Integer number) {
 
+    }
+
+
+
+
+    public List<String> startTab(CommandSender sender,String[] args){
+        if(args.length == 0){
+            return List.of("test","test");
+        }else{
+            return List.of();
+        }
     }
 
 }
